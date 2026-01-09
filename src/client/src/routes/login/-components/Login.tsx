d2152f65e011865d48094ec127e5cd7cdf14dc4d
susanpant7 <susan.pant7@gmail.com>
2026-01-09 17:23:18 -0500

    installed shadcn ui component

--- src/client/src/routes/login/-components/Login.tsx
+++ src/client/src/routes/login/-components/Login.tsx
@@ -1,8 +1,11 @@
+import {Button} from "@/components/ui/button.tsx";
 
 const Login = () => {
     return (
         <div>
-            Hello
+            <div className="flex min-h-svh flex-col items-center justify-center">
+                <Button>Click me</Button>
+            </div>
         </div>
     );
 };
--- src/client/vite.config.ts
+++ src/client/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
@@ -14,4 +15,9 @@ export default defineConfig({
       },
     }),
   ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 })
