2f5cc04e1968f5133bb08ddd996975a0e507ca50
susanpant7 <susan.pant7@gmail.com>
2026-01-11 23:06:40 -0500

    migration for user otp table

---
+++ src/server/NepalVotes.Infrastructure/Persistence/Migrations/20260112040507_user_otp_entity_created.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NepalVotes.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class user_otp_entity_created : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UserOtps",
+                columns: table => new
+                {
+                    UserOtpId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    HashedOtpCode = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
+                    ExpiryDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    IsUsed = table.Column<bool>(type: "bit", nullable: false, defaultValue: false),
+                    AttemptCount = table.Column<int>(type: "int", nullable: false, defaultValue: 0),
+                    UserOtpType = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserOtps", x => x.UserOtpId);
+                    table.ForeignKey(
+                        name: "FK_UserOtps_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_UserOtps_UserId",
+                table: "UserOtps",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UserOtps");
+        }
+    }
+}
