13d298bf9011efd46a6e96160c52bf677832f173
susanpant7 <susan.pant7@gmail.com>
2026-01-19 16:20:24 -0500

    added missed file

---
+++ src/server/NepalVotes.Infrastructure/Persistence/Migrations/20260119211708_makeTheConstituencyIdInWardTableNotRequired.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NepalVotes.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class makeTheConstituencyIdInWardTableNotRequired : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ConstituencyId",
+                table: "Wards",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "ConstituencyId",
+                table: "Wards",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+        }
+    }
+}
