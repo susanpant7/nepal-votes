0a789441570f5cb63b1317d9e8bfb507ba2e714a
susanpant7 <susan.pant7@gmail.com>
2026-02-06 19:26:04 -0500

    server api url from frontend

--- src/client/src/lib/app.settings.ts
+++ src/client/src/lib/app.settings.ts
@@ -1,8 +1,8 @@
 export const APP_SETTINGS = {
-  SERVER_URL: "http://localhost:5119/",
-  AUTH_SERVER_URL: "http://localhost:5119/",
-  // SERVER_URL:
-  //   "https://nepalvotes-h3egfccwfvd0gycg.canadaeast-01.azurewebsites.net/",
-  // AUTH_SERVER_URL:
-  //   "https://nepalvotes-h3egfccwfvd0gycg.canadaeast-01.azurewebsites.net/",
+  // SERVER_URL: "http://localhost:5119/",
+  // AUTH_SERVER_URL: "http://localhost:5119/",
+  SERVER_URL:
+    "https://nepalvotes-h3egfccwfvd0gycg.canadaeast-01.azurewebsites.net/",
+  AUTH_SERVER_URL:
+    "https://nepalvotes-h3egfccwfvd0gycg.canadaeast-01.azurewebsites.net/",
 } as const;
